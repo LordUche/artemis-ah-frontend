eefb67a732c34e2605e4a9e650ea6b167e979fd0
Chrismarcel <cmarcelj@gmail.com>
2019-04-02 18:09:08 +0100

    chore(test): set up unit and e2e tests
    - install jest and jest dependencies
    - install nightwatch and necessary dependencies
    - write e2e tests
    - write jest tests
    - ensure tests are passing
    
    [Finishes #164198446]

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 //Views
 import LandingPage from './views/LandingPage.jsx';
 import ProfilePage from './views/ProfilePage.jsx';
 
 const App = () => {
-  return(
+  return (
     <BrowserRouter>
       <Switch>
-        <Route exact path ="/" component={LandingPage}/>
-        <Route exact path ="/profile" component={ProfilePage}/>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/profile" component={ProfilePage} />
       </Switch>
     </BrowserRouter>
   )
