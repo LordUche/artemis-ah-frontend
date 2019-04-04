100d3b7d3c57438f07282029ac6489807349f58e
Adaeze Odurukwe <adaezeeze-odurukwe@MacBook-Pro-2.local>
2019-04-04 16:46:34 +0100

    create server
    [finishes #165119232]

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,73 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCSSExtractPlugin = require('mini-css-extract-plugin'); 
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
-module.exports = env => {
-  return {
-    entry: './src/App.jsx',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
-    },
-    resolve: {
-      extensions: ['.js', '.jsx', '.json', '.css']
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader', {
-            loader: 'eslint-loader',
-            options: {
-              useEslintrc: true,
-            }
-          }]
-        },
-        {
-          test: /\.html$/,
-          use: {
-            loader: 'html-loader',
-            options: { minimize: true }
+module.exports = env => ({
+  entry: './src/App.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.css']
+  },
+  node: {
+    net: 'empty',
+    fs: 'empty'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', {
+          loader: 'eslint-loader',
+          options: {
+            useEslintrc: true,
           }
-        },
-        {
-          test: /\.(css|scss)$/,
-          use: [
-            'style-loader',
-            MiniCSSExtractPlugin.loader,
-            'css-loader',
-            'sass-loader'
-          ]
+        }]
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+          options: { minimize: true }
         }
-      ]
-    },
-    devtool: env.production ? 'source-maps' : 'eval',
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './public/index.html',
-        filename: 'index.html'
-      }),
-      new MiniCSSExtractPlugin({
-        filename: './style.css'
-      })   
-    ],
-    devServer: {
-      historyApiFallback: true
-    }
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          'style-loader',
+          MiniCSSExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devtool: env.production ? 'source-maps' : 'eval',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    }),
+    new MiniCSSExtractPlugin({
+      filename: './style.css'
+    })
+  ],
+  devServer: {
+    historyApiFallback: true
   }
-};
+});
